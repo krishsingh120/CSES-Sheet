2d464921dc36d153ce1fd4fb09a27edc67af5d87
Krish Singh <154358180+krishsingh120@users.noreply.github.com>
2026-04-21 14:24:21 +0530

    Feat: Added sliding window mode problem

--- SlidingWindow/SlidingWindowMode.java
+++ SlidingWindow/SlidingWindowMode.java
@@ -2,7 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.TreeSet;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+/*
+* Approach:
+  1. req:
+        FreqMap: <Elem, Freq>
+        Inv FreqMap: <freq, Set<elem>>
+
+  2. first handle removing element freq in both maps.
+  3. then handle upcoming element freq in both maps.
+  4. Ans => Last freq's first element.
+
+*/
 
 public class SlidingWindowMode {
 
@@ -11,7 +26,57 @@ public class SlidingWindowMode {
    */
 
   public static void distArr(int n, int k, int nums[], int ans[]) {
-    
+
+    Map<Integer, Integer> freq = new HashMap<>();
+    PriorityQueue<int[]> pq = new PriorityQueue<>(
+        (a, b) -> {
+          if (a[0] != b[0])
+            return b[0] - a[0]; // max freq first
+          return a[1] - b[1]; // smaller element first
+        });
+
+    // first window
+    for (int i = 0; i < k; i++) {
+      freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+    }
+
+    // push all into heap
+    for (int key : freq.keySet()) {
+      pq.add(new int[] { freq.get(key), key });
+    }
+
+    ans[0] = getMode(pq, freq);
+
+    // sliding window
+    for (int i = k; i < n; i++) {
+
+      int out = nums[i - k];
+      freq.put(out, freq.get(out) - 1);
+      if (freq.get(out) == 0)
+        freq.remove(out);
+      else
+        pq.add(new int[] { freq.get(out), out });
+
+      int in = nums[i];
+      freq.put(in, freq.getOrDefault(in, 0) + 1);
+      pq.add(new int[] { freq.get(in), in });
+
+      ans[i - k + 1] = getMode(pq, freq);
+    }
+  }
+
+  // lazy removal
+  private static int getMode(PriorityQueue<int[]> pq, Map<Integer, Integer> freq) {
+    while (true) {
+      int[] top = pq.peek();
+      int f = top[0];
+      int val = top[1];
+
+      if (freq.getOrDefault(val, 0) == f) {
+        return val;
+      }
+      pq.poll(); // remove stale
+    }
 
   }
 
